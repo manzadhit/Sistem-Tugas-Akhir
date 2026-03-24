93d6f6d849b5a29627aec95a6df8785c68bfe2e1
I Nyoman Aditia <aadhit407@gmail.com>
2026-03-24 20:42:21 +0800

    feat: create CosineSimilarityService

---
+++ app/Services/CBF/CosineSimilarityService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services\CBF;
+
+class CosineSimilarityService
+{
+  public function calculate($vectorA, $vectorB)
+  {
+    $magA = $this->magnitude($vectorA);
+    $magB = $this->magnitude($vectorB);
+
+    if ($magA === 0.0 || $magB === 0.0) {
+      return 0.0;
+    }
+
+    $dotProduct = $this->dotProduct($vectorA, $vectorB);
+
+    return $dotProduct / ($magA * $magB);
+  }
+
+  public function calculateAll($tfidf)
+  {
+    $scores = [];
+
+    $query = $tfidf['judul_ta'];
+    $docs = $tfidf;
+    unset($docs['judul_ta']);
+
+    foreach ($docs as $dosenId => $vector) {
+      $scores[$dosenId] = $this->calculate($query, $vector);
+    }
+
+    return arsort($scores);
+  }
+
+  protected function dotProduct($vectorA, $vectorB)
+  {
+    $result = 0.0;
+
+    foreach ($vectorA as $term => $valueA) {
+      if (isset($vectorB[$term])) {
+        $result += $valueA * $vectorB[$term];
+      }
+    }
+
+    return $result;
+  }
+
+  protected function magnitude($vector)
+  {
+    $sumOfSquares = 0.0;
+
+    foreach ($vector as $term => $value) {
+      $sumOfSquares += $value * $value;
+    }
+
+    return sqrt($sumOfSquares);
+  }
+}
